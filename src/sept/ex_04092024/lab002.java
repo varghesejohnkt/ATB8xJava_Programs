17b3e3bfb4a9550196a1937a0f13fc16562d06e8
Varghese John <varghesejohnkt@gmail.com>
2024-09-08 23:43:24 +0530

    latest codes

--- src/sept/ex_04092024/lab002.java
+++ src/sept/ex_04092024/lab002.java
@@ -1,4 +1,10 @@
 package sept.ex_04092024;
 
 public class lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2*2);
+        System.out.println(2+2);
+
+    }
 }
---
+++ src/sept/ex_04092024/lab003.java
@@ -0,0 +1,7 @@
+package sept.ex_04092024;
+
+public class lab003 {
+}
+//Single line comments added
+/*Multi line comments added
+     yes multiline   */
