118a965c89182a0bc974c0d241f2ea7fa9252185
Varghese John <varghesejohnkt@gmail.com>
2024-09-18 23:14:38 +0530

    Tasks for 13th Sep

---
+++ src/sept/task_13_1/task1table.java
@@ -0,0 +1,20 @@
+package sept.task_13_1;
+
+public class task1table {
+    public static void main(String[] args){
+        int num =10;
+        char line = '\n';
+        System.out.printf("%d * %d = %d %n",num ,1 ,num);
+        System.out.printf("%d * %d = %d %n",num ,2 ,num * 2);
+        System.out.printf("%d * %d = %d %n",num ,3 ,num * 3);
+        System.out.printf("%d * %d = %d %n",num ,4 ,num * 4);
+        System.out.printf("%d * %d = %d %n",num ,5 ,num * 5);
+        System.out.printf("%d * %d = %d %n",num ,6 ,num * 6);
+        System.out.printf("%d * %d = %d %n",num ,7 ,num * 7);
+        System.out.printf("%d * %d = %d %n",num ,8 ,num * 8);
+        System.out.printf("%d * %d = %d %n",num ,9 ,num * 9);
+        System.out.printf("%d * %d = %d %n",num ,10 ,num * 10);
+
+
+    }
+}
---
+++ src/sept/task_13_1/task2calculator.java
@@ -0,0 +1,16 @@
+package sept.task_13_1;
+
+public class task2calculator {
+    public static void main(String[]args){
+        double a = 10;
+        double b = 34;
+        System.out.printf("sum            : %f + %f = %f %n",a,b,a+b);
+        System.out.printf("Difference     : %f - %f = %f %n",a,b,a-b);
+        System.out.printf("Multiplication : %f * %f = %f %n",a,b,a-b);
+        System.out.printf("Division       : %f / %f = %f %n",a,b,a/b);
+        System.out.printf("Modulus        : %f & %f = %f %n",a,b,a%b);
+
+
+
+    }
+}
